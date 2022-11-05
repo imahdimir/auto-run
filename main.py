9104937350e06caa617c9e8bbe330c34dc241bc1
imahdimir <imahdimir@gmail.com>
2022-11-05 11:59:45 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+
+    """
+
+from pathlib import Path
+
+
+def main():
+
+    pass
+
+    ##
+    print(Path.cwd())
+
+
+    ##
+
+
+
+##
+
+
+if __name__ == '__main__':
+    main()
+    print(f'{Path(__file__).name} Done!')
